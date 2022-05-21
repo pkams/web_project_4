9450d1f647680470aa9bfabd431597828f411283
pkams <patrick_ams@hotmail.com>
2022-05-21 16:13:29 -0300

    finished javascript implementation

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+// Popup actions
 let edit_button = document.querySelector('.profile__edit-button')
 let close_button = document.querySelector('.popup__close')
 let popup = document.querySelector('.popup')
+
+let submit_button = document.querySelector('.popup__save-button')
 
 function editButton(event){
     event.preventDefault();
@@ -12,5 +15,24 @@ function closeButton(event){
     popup.classList.add('popup__container_popup_opened')
 }
 
+function submitButton(event){
+    event.preventDefault()
+
+    // Get forms and profile variables
+    let name = document.querySelector('#name')
+    let job = document.querySelector('#job')
+    let profile_name = document.querySelector('.profile__name')
+    let profile_job = document.querySelector('.profile__job')
+
+    // Switch actual profile information for the new information
+    profile_name.innerText = name.value
+    profile_job.innerText = job.value
+
+    // Erase values in forms field
+    name.value = ''
+    job.value = ''
+}
+
 edit_button.addEventListener('click', editButton)
 close_button.addEventListener('click', closeButton)
+submit_button.addEventListener('click', submitButton)
